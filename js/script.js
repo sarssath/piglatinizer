706ca62d72ab429e44dee5737233d4ee05efc145
sarssath <srarssath@gmail.com>
2019-10-31 21:53:30 +0000

    Added Words

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ $( "document" ).ready(function() { //this helps your jQuery to work. Write all o
 $("#button").click(function(){
     $(".class").text();
     var vowels = ["a", "e", "i", "o", "u"];
-    var input = "hello";
+    var input = $(".input").val();
     var split = input.split("");
     var firstLetter = split[0];
+    console.log(input);
+    if (vowels.includes(firstLetter)) {
+        $(".output").append(input + "ay");
+    } else {
+        input.substring(0,0);
+        console.log(input.substring(0,0))
+        $(".output").append(input + firstLetter + "ay");
+    }
 });
 
 
